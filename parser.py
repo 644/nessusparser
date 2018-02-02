eaedc018b041d39a22df7223461b9ca8d4df9a73
blackcat <moroccanhashish@gmail.com>
2018-02-02 01:55:47 +0000

    Updated file

--- parser.py
+++ parser.py
@@ -1,21 +1,21 @@
 import xml.etree.ElementTree as ET
 import sqlite3
+
 conn = sqlite3.connect('reports.db')
 c = conn.cursor()
 tree = ET.parse('report.xml').getroot()
-c.execute("CREATE TABLE IF NOT EXISTS reports (ReportItem text, cwe text, description text)")
 
-inserts = []
+c.execute("CREATE TABLE IF NOT EXISTS reports (ReportItem text, cwe text, description text)")
 for ReportItem in tree.iter('ReportItem'):
 	for e in ReportItem.iter():
-		fields = "\'" + '\', \''.join(e.tag) + "\'"
-		data = "\'" + '\', \''.join(e.text) + "\'"
-		
-		if len(e.text) > 3:
-			marklist = ["?"] * len(e.text)
-			marks = ', '.join(marklist)
-			print(e.text)
-			c.execute("INSERT INTO reports (cwe) VALUES (?)", (e.text,))
+		## Possibly attempt trickery with data entry
+		#fields = "\'" + '\', \''.join(e.tag) + "\'"
+		#data = "\'" + '\', \''.join(e.text) + "\'"
+		#if len(e.text) > 3:
+		#	marklist = ["?"] * len(e.text)
+		#	marks = ', '.join(marklist)
+		print(e.text)
+		c.execute("INSERT INTO reports (cwe) VALUES (?)", (e.text,))
 
 conn.commit()
 conn.close()
